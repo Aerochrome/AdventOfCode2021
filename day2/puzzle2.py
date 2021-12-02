f629c9ce5630c664600165ef887327c10e26c449
Philipp Lätsch <p.laetsch@outlook.de>
2021-12-03 00:54:47 +0100

    day 2 - puzzle 2

---
+++ day2/puzzle2.py
@@ -0,0 +1,38 @@
+filepath = "input.txt"
+puzzle_input = []
+
+with open(filepath) as file:
+    for line in file:
+        puzzle_input.append(line.rstrip())
+
+
+def run_instructions(instructions):
+    state = {
+        'x': 0,
+        'y': 0,
+        'aim': 0
+    }
+
+    for instruction in instructions:
+        splitted = instruction.split(" ", 2)
+        run_instruction(splitted[0], int(splitted[1]), state)
+
+    return state
+
+
+def run_instruction(instruction, value, state):
+    if instruction == "up":
+        state['aim'] -= value
+    elif instruction == "down":
+        state['aim'] += value
+    elif instruction == "forward":
+        state['x'] += value
+        state['y'] += state['aim'] * value
+
+
+def multiply_state(state):
+    return state['x'] * state['y']
+
+
+result_state = run_instructions(puzzle_input)
+print(multiply_state(result_state))
